e7cb697fd4d4df8c021f6f2f9bcf2a4add22ddf3
clong desktop <chip_long@yahoo.com>
2023-03-04 20:22:07 -0700

    added Add Budget Form

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,10 @@
+import { Budget } from "./types";
+
+// does multiplication on an HSL color function -- this is not actually returning HSL
+// 34 is a magic number (increments the hue)
+const generateRandomColor = (existingBudgetsLength: number) =>
+  `${existingBudgetsLength * 34} 65% 50%`;
+
 export const fetchData = (key: string) => {
   const storage = localStorage.getItem(key) as any;
 
@@ -6,3 +13,26 @@ export const fetchData = (key: string) => {
 
 export const deleteItem = ({ key }: { key: string }) =>
   localStorage.removeItem(key);
+
+export const createBudget = ({
+  name,
+  amount,
+}: {
+  name: string;
+  amount: number;
+}) => {
+  const existingBudgets: Budget[] = fetchData("budgets") ?? [];
+
+  const newItem = {
+    id: crypto.randomUUID(),
+    name,
+    amount: amount,
+    color: generateRandomColor(existingBudgets.length),
+    createdAd: Date.now(),
+  };
+
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
+};
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,34 +1,76 @@
 import { ActionFunctionArgs, useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 import Intro from "../components/Intro";
-import { fetchData } from "../helpers";
+import { createBudget, fetchData } from "../helpers";
 
 type dashboardLoaderData = {
   userName: string | null;
+  budgets: string | null;
 };
 
 export const dashboardLoader = () => {
   const userName = fetchData("userName") as string | null;
+  const budgets = fetchData("budgets") as string | null;
 
-  return { userName };
+  return { userName, budgets };
 };
 
 export const dashboardAction = async ({ request }: ActionFunctionArgs) => {
   const data = await request.formData();
-  const formData = Object.fromEntries(data);
+  const { _action, ...values } = Object.fromEntries(data);
 
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName));
-    return toast.success(`Welcome ${formData.userName}`);
-  } catch (error) {
-    throw new Error("There was a problem creating your account.");
+  switch (_action) {
+    case "newUser":
+      try {
+        localStorage.setItem("userName", JSON.stringify(values.userName));
+        return toast.success(`Welcome ${values.userName}`);
+      } catch (e) {
+        throw new Error("There was a problem creating your account.");
+      }
+      break;
+    case "createBudget":
+      try {
+        createBudget({
+          name: values.newBudget as string,
+          amount: +(values.newBudgetAmount as string),
+        });
+        return toast.success("Budget created!");
+      } catch (e) {
+        throw new Error("There was a problem creating your budget.");
+      }
+      break;
+
+    default:
+      throw new Error(`${_action} is not a recognized form.`);
+      break;
   }
 };
 
 function Dashboard() {
-  const { userName } = useLoaderData() as dashboardLoaderData;
+  const { userName, budgets } = useLoaderData() as dashboardLoaderData;
 
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {/* {budgets ? ():() } */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 }
 
 export default Dashboard;
